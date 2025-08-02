6c64219ee102aa84e275fec07be3673a30f9bb21
Emjey <reach2jeyan@outlook.com>
2025-08-02 12:27:23 +0530

    remove stray prints

--- pytest_html_plus/compute_report_metadata.py
+++ pytest_html_plus/compute_report_metadata.py
@@ -7,7 +7,6 @@ from pytest_html_plus.utils import is_main_worker, get_env_marker, get_git_branc
 def write_plus_metadata_if_main_worker(config, output_path="plus_metadata.json"):
     if not is_main_worker():
         return
-    print(get_env_marker(config))
     metadata = {
         "report_title": "Automated Test Report",
         "environment": get_env_marker(config),
@@ -15,8 +14,6 @@ def write_plus_metadata_if_main_worker(config, output_path="plus_metadata.json")
         "commit": get_git_commit(),
         "generated_at": datetime.now().isoformat()
     }
-    print(metadata)
-
     with open(output_path, "w") as f:
         print(metadata)
         json.dump(metadata, f, indent=2)
