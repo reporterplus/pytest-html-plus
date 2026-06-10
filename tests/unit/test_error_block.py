215ed910cfa862dcde54a6cb4686ec94242673ca
Emjey <reach2jeyan@outlook.com>
2026-06-10 11:53:16 +0000

    fix lint errors

--- tests/unit/test_error_block.py
+++ tests/unit/test_error_block.py
@@ -1,16 +1,20 @@
-from pytest_html_plus.utils import extract_error_block
 import pytest
+
+from pytest_html_plus.utils import extract_error_block
+
 
 def test_error_block_basic():
     error = "line 1\nE AssertionError: something went wrong\nline 3\nE ValueError: another error"  # noqa
     expected = "E AssertionError: something went wrong\nE ValueError: another error"
     assert extract_error_block(error) == expected
 
+
 @pytest.mark.jira("https://acme.atlassian.net/browse/QA-123")
 def test_error_block_spaces_and_blank_lines():
     error = "\n\n   E TypeError\n\n   \nE IndexError\n"
     expected = "E TypeError\nE IndexError"
     assert extract_error_block(error) == expected
+
 
 @pytest.mark.link("https://acme.atlassian.net/browse/PAY-456")
 def test_error_block_no_E_lines():
